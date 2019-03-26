90be7ca4ef70680fe581f055345ddbb7ba5e4909
NKBelousov <belousovnikita92@gmail.com>
2019-03-27 02:04:36 +0300

    add appbar with search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import { inject } from "mobx-react";
-import { Component } from "react";
+import { CssBaseline } from "@material-ui/core";
+import { inject, observer } from "mobx-react";
+import React, { Component } from "react";
+
+import SearchAppBar from "./components/SearchAppBar";
 
 @inject("pokemonStore")
-export default class App extends Component {
+@observer
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onSearchChange(event) {
+    const { value } = event.target;
+    this.props.pokemonStore.setSearch(value);
+  }
   render() {
-    return "Hello, Pokedex!";
+    return (
+      <>
+        <CssBaseline />
+        <SearchAppBar
+          onChange={this.onSearchChange}
+          value={this.props.pokemonStore.search}
+        />
+      </>
+    );
   }
 }
+
+export default App;
--- src/stores/pokemonsStore.js
+++ src/stores/pokemonsStore.js
@@ -1,3 +1,11 @@
-export class PokemonsStore {}
+import { action, observable } from "mobx";
+
+export class PokemonsStore {
+  @observable search = "";
+
+  @action setSearch(value) {
+    this.search = value.trim();
+  }
+}
 
 export default new PokemonsStore();
