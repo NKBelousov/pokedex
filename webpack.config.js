cdf4c5a0bb21c3ac158baa76e8c362a2402de85d
NKBelousov <belousovnikita92@gmail.com>
2019-03-27 01:16:06 +0300

    setup dependencies & build process

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve('src/index.js'),
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve('dist'),
+    historyApiFallback: true,
+    hot: true,
+    index: path.resolve('dist/index.html'),
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.ejs'
+    })
+  ]
+}
